57f81e7e234b206c328be01b2ed3312fee9fad76
Trong Truong <nahn.truong@gmail.com>
2013-01-16 00:59:43 -0500

    Added tables tag and posttag to db

--- journal/models.py
+++ journal/models.py
@@ -13,3 +13,17 @@ class Post(models.Model):
     def __unicode__(self):
         return "{0}/{1}: {2}".format(self.subject, self.title, self.body)
 
+class Tag(models.Model):
+    name = models.CharField(max_length=222)
+    num_posts = models.IntegerField()
+
+    def __unicode__(self):
+        return "{0}: {1}".format(self.name, self.num_posts)
+
+class PostTag(models.Model):
+    post = models.ForeignKey(Post)
+    tag = models.ForeignKey(Tag)
+
+    def __unicode__(self):
+        return "{0}: {1}".format(self.post, self.tag)
+
