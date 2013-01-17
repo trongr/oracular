894a5cfaa650d99eb6d2711c28ecbd79225ad1eb
Trong Truong <nahn.truong@gmail.com>
2013-01-16 22:06:53 -0500

    Fixed width 100% overflow problem

--- views/js/home.js
+++ views/js/home.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+    $("#new_post_button").click(function(){create_post(); return false;});
+    $("#random_post").click(function(){edit_random_post(); return false;});
+});
+
 function edit_random_post(){
     $("#random_post").css({
         "display": "none",
@@ -5,5 +10,9 @@ function edit_random_post(){
     $("#random_edit_form").css({
         "display": "block",
     });
-    $("textarea.title").focus();
+    $("input.title").focus();
+}
+
+function create_post(){
+    alert("Hello");
 }
