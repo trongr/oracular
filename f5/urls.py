29270290a8f961bbd6ce4ca6b83267b93aaa446d
Trong Truong <nahn.truong@gmail.com>
2013-01-27 09:00:47 -0500

    changed f5/urls.py to fix routing again

--- f5/urls.py
+++ f5/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'f5.views.home', name='home'),
-    url(r'^/', include('journal.urls', namespace='journal')),
+    url(r'^journal/', include('journal.urls', namespace='journal')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
